948bffbcf1dfc32bdc473591a069c1040a4e6c10
walter-rothlin <walter@rothlin.com>
2024-12-01 12:46:07 +0000

    Test Script added

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # RaspberryPi4PiPlates
 
-import piplates.RELAYplate as RELAY
+Pi Image: Pi OS Legacy 64Bit 2024-10-22
+
+1. Install Pip
+    ```
+    sudo apt-get install python3-pip
+    ```
+
+2. Enable SPI
+    ```
+    sudo raspi-config
+    ```
+    [Enable SPI Hardware](https://www.raspberrypi-spy.co.uk/2014/08/enabling-the-spi-interface-on-the-raspberry-pi/)
+
+3. Install requirements
+    ```
+    pip install -r requirements.txt
+    ```
--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 import piplates.RELAYplate as Relay
+import time
 
+PLATE_ADDRESS = 0
+
+print(f"Testing Plate on address {PLATE_ADDRESS}")
 # Relay control
-Relay.relayON(0, 2)  # Activate relay at address 0, relay 2
+for i in range(1,8):
+    Relay.relayON(PLATE_ADDRESS,i)
+    print(f"Relay ON: {PLATE_ADDRESS}, {i}")
+    time.sleep(1)
+
+for i in range(1,8):
+    Relay.relayOFF(PLATE_ADDRESS,i)
+    print(f"Relay OFF: {PLATE_ADDRESS}, {i}")
+    time.sleep(1)
+
+
+print("Done")
