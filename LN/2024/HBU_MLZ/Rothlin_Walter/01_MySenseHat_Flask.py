f270aed04ad9a7372a7c7d0d33950380aff58220
walter-rothlin <walter@rothlin.com>
2025-01-07 19:32:31 +0000

    Updated

--- LN/2024/HBU_MLZ/Rothlin_Walter/01_MySenseHat_Flask.py
+++ LN/2024/HBU_MLZ/Rothlin_Walter/01_MySenseHat_Flask.py
@@ -13,8 +13,9 @@
 # 17-Dec-2024   Walter Rothlin      Initial Version
 # 18-Dec-2024   Walter Rothlin      Added show_message(), set_rotation(), show_letter(), low_light()
 # 19-Dec-2024   Walter Rothlin      Added clear_LED()
+# 07-Jan-2025   Walter Rothlin      Added TestForms for all functions
 # ------------------------------------------------------------------
-version = "01.00.03"
+version = "Walter Rothlin V1.0"
 
 from flask import *
 from Class_MySenseHat import *
@@ -35,8 +36,8 @@ def set_pixel():
     b = all_get_parameters.get('b', '0')
     
     
-    sense.set_pixel(x,y,r,g,b)   
-    return f'set_pixel({x},{y},{r},{g},{b})!'
+    sense.set_pixel(x,y,r,g,b)
+    return render_template('test_endpoints_page.html', test_result_string=f'set_pixel({x},{y},{r},{g},{b})!', called_end_point='set_pixel', parameters=all_get_parameters)  
 
 @app.route('/get_status', methods=['GET'])
 def get_status():
@@ -55,7 +56,7 @@ def get_status():
 @app.route('/clear_LED', methods=['GET'])
 def clear_LED():
     all_get_parameters = dict(request.args.items())
-    # print(all_get_parameters)
+    print(all_get_parameters)
     bg_color = all_get_parameters.get('color', 'black')
     if bg_color == 'black':
         sense.clear(MySenseHat.black)
@@ -69,7 +70,7 @@ def clear_LED():
         sense.clear(MySenseHat.white) 
     elif bg_color == 'grey':
         sense.clear(MySenseHat.grey)
-    return render_template('test_endpoints_page.html', test_result_string=f'Clear LED matrix to color {bg_color}!')
+    return render_template('test_endpoints_page.html', test_result_string=f'Clear LED matrix to color {bg_color}!', called_end_point='clear_LED', parameters=all_get_parameters)
 
 @app.route('/show_message', methods=['GET'])
 def show_message():
@@ -78,7 +79,7 @@ def show_message():
     text_to_show = all_get_parameters.get('text_string', 'Nothing')
     scroll_speed = float(all_get_parameters.get('scroll_speed', '0.1'))
     sense.show_message(text_to_show, scroll_speed=scroll_speed)
-    return render_template('test_endpoints_page.html', test_result_string=f'text_string("{text_to_show}", scroll_speed={scroll_speed})!')  
+    return render_template('test_endpoints_page.html', test_result_string=f'text_string("{text_to_show}", scroll_speed={scroll_speed})!', called_end_point='show_message', parameters=all_get_parameters)  
 
 @app.route('/show_letter', methods=['GET'])
 def show_letter():
@@ -89,7 +90,7 @@ def show_letter():
     back_colour = eval(all_get_parameters.get('back_colour', '(100,100,100)'))
     print(f'show_letter({letter_to_show}, text_colour={text_colour}, back_colour={back_colour})')
     sense.show_letter(letter_to_show, text_colour=text_colour, back_colour=back_colour)  
-    return render_template('test_endpoints_page.html', test_result_string=f'show_letter({letter_to_show}, text_colour={text_colour}, back_colour={back_colour})')  
+    return render_template('test_endpoints_page.html', test_result_string=f'show_letter({letter_to_show}, text_colour={text_colour}, back_colour={back_colour})', called_end_point='show_letter', parameters=all_get_parameters)  
     
 
 @app.route('/low_light', methods=['GET'])
@@ -104,7 +105,7 @@ def low_light():
         low_light_val = False
     print(f'(3) low_light={low_light_val} (type(low_light_val)={type(low_light_val)})')
     sense.low_light=low_light_val
-    return render_template('test_endpoints_page.html', test_result_string=f'low_light = {low_light_val}')  
+    return render_template('test_endpoints_page.html', test_result_string=f'low_light = {low_light_val}', called_end_point='low_light', parameters=all_get_parameters)  
 
 @app.route('/set_rotation', methods=['GET'])
 def set_rotation():
@@ -113,7 +114,7 @@ def set_rotation():
     r = int(all_get_parameters.get('r', '90'))
     redraw = bool(all_get_parameters.get('redraw', 'True'))
     sense.set_rotation(r=r, redraw=redraw)
-    return render_template('test_endpoints_page.html', test_result_string=f'set_rotation({r}, redraw={redraw})!')    
+    return render_template('test_endpoints_page.html', test_result_string=f'set_rotation({r}, redraw={redraw})!', called_end_point='set_rotation', parameters=all_get_parameters)    
 
 @app.route('/')
 def index():
