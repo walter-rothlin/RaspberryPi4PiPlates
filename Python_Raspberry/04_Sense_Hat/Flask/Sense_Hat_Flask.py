be360e95419264c4d527415b6f45fa1fe0d37563
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2025-07-08 11:07:58 +0200

    Update

---
+++ Python_Raspberry/04_Sense_Hat/Flask/Sense_Hat_Flask.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name  : Sense_Hat_Flask.py
+#
+# Description: Stellt Web-Applikationen und REST-Services für die Steuerung der LED-Matrix
+#              und das Auslesen der Sensoren auf dem Sense-Hat
+
+#
+#
+# Autor: Walter Rothlin
+#
+# History:  
+# 01-Jul-2025   Walter Rothlin      Initial Version
+# ------------------------------------------------------------------
+
+from flask import *
+from sense_hat import SenseHat
+from time import sleep
+
+red   = (255,   0,   0)
+blue  = (  0,   0, 255)
+green = (  0, 255,   0)
+black = (  0,   0,   0)
+
+
+app = Flask(__name__)
+sense = SenseHat()
+## sense = MySenseHat()
+
+@app.route('/clear_LED', methods=['GET'])
+def clear_LED():
+    all_get_parameters = dict(request.args.items())
+    # print(all_get_parameters)
+    bg_color = all_get_parameters.get('color', 'black')
+    if bg_color == 'black':
+        sense.clear()
+    elif bg_color == 'red':
+        sense.clear(red) 
+    elif bg_color == 'blue':
+        sense.clear(blue)
+    elif bg_color == 'green':
+        sense.clear(green)   
+    return f'Clear LED matrix to color {bg_color}!'
+    
+@app.route('/get_status', methods=['GET'])
+def get_status():
+    pixel_status = sense.get_pixels()
+    humidity = sense.get_humidity()
+    temperature = sense.get_temperature()
+    pressure = sense.get_pressure()
+    
+    # print(pixel_status)
+    return {'LED_Matrix': pixel_status,
+            'Temperature': temperature,
+            'Humidity': humidity,
+            'Pressure': pressure,
+           }
+
+@app.route('/set_pixel', methods=['GET'])
+def set_pixel():
+    all_get_parameters = dict(request.args.items())
+    print(all_get_parameters)
+    x = int(all_get_parameters.get('x', '0'))
+    y = int(all_get_parameters.get('y', '0'))
+    r = int(all_get_parameters.get('r', '0'))
+    g = int(all_get_parameters.get('g', '0'))
+    b = int(all_get_parameters.get('b', '0'))
+    
+    
+    sense.set_pixel(x,y,r,g,b)   
+    return f'set_pixel({x},{y},{r},{g},{b})!'
+
+
+@app.route('/wetter_json')
+def get_weather_as_JSON():
+    weather_data = {
+    'Temperatur':sense.get_temperature(),
+    'Luftdruck':sense.get_pressure(),
+    'Feuchtigkeit':sense.get_humidity(),
+    }
+    return weather_data
+    
+
+@app.route('/weather')
+def get_weather():
+    return f'''
+    <h1>Wetter</h1>
+    Temperatur: {sense.get_temperature():0.2f}°C<br/>
+    Luftdruck: {sense.get_pressure():0.2f}mBar<br/>
+    Rel. Feuchtigkeit: {sense.get_humidity():0.2f}%<br/>
+    '''
+    
+    
+@app.route('/')
+def show_index():
+    return f'''
+    <h1>INDEX for {say_hello()}</h1>
+    <ul>
+        <li><a href="/weather">Wetter</a> (as HTML)</li>
+        <li><a href="/wetter_json">Wetter</a> (as JSON)</li>
+        <li><a href="/get_status">SenseHat Status</a></li>
+    </ul>
+    <ul>
+        <li><a href="/clear_LED?color=black">Clear-LED</a> (color=black)</li>
+        <li><a href="/set_pixel?x=0&y=0&r=255&g=0&b=0">Set-Pixel</a> (x=0, y=0, r=255, g=0, b=0)</li>
+
+
+    </ul>
+    '''
+    
+@app.route('/inde_template')
+def index():
+    return render_template('index.html')
+    
+    
+@app.route('/hello')    
+def say_hello():
+    return 'BZU V1.0'
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, host='192.168.107.97', port=5001)
---
+++ Python_Raspberry/Bahnhofuhr/Python_On_Resbarry_3/SBB_Uhr.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name  : SBB_Uhr.py
+#
+# Description: Generiert den Minuten-Clock um die Mutteruhr zu simulieren. Stellt Web-Applikationen und 
+#              REST-Services fürs richten der Uhr zur Verfügung
+#
+# Autor: Raphael Sauer
+#
+# History:  
+# 01-Jul-2025   Raphael Sauer      Initial Version
+# ------------------------------------------------------------------
+from  flask import *
+import threading
+import RPi.GPIO as GPIO
+import time
+
+# setup REST API
+app = Flask(__name__)
+
+# Pin-Definition (BCM-Nummerierung)
+PIN = 26  
+
+# Anfangszustand
+zustand = False
+
+def switchGPIO():
+    global zustand
+    zustand = not zustand
+    GPIO.output(PIN, zustand)
+
+@app.route("/")
+def Welcome():
+    return '''
+<html>
+<head>
+<title>Uhr Controller</title>
+</head>
+<body>
+
+<h1>Uhr Controller</h1>
+<a href="/legal">Copyright</a><br>
+<p>Unten ist die API beschreibung</p>
+<a href="/tick">/tick (ruecke die uhr einen tick (1Min) vor)</a><br>
+<a href="/tick_args?count=10&delay=0.125">/tick_args?count=10&delay=0.125 (ruecke die Uhr um {count} weiter in einem zeitabstand von {delay} sekunden)</a>
+</body>
+</html>
+'''
+
+@app.route("/legal")
+def Legal():
+   return '''
+<html>
+<head>
+<title>Legal</title>
+</head>
+<body>
+
+<h1>Copyright</h1>
+<p>Copyright ┬® 2025 Raphael Sauer</p>
+<p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ÔÇ£SoftwareÔÇØ), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+<p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+<p>THE SOFTWARE IS PROVIDED ÔÇ£AS ISÔÇØ, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+</body>
+</html>
+'''
+
+@app.route("/tick")
+def tick():
+    switchGPIO()
+    return "tick executed"
+
+@app.route("/tick_args", methods=['GET'])
+def tickArgs():
+    count = int(request.args.get('count'))
+    delay = float(request.args.get('delay'))
+    for x in range(count):
+        switchGPIO()
+        time.sleep(delay)
+    return "done"
+
+def mainThread():
+    global PIN
+    global zustand
+    # Setup GPIO
+    #GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(PIN, GPIO.OUT)
+
+    # Anfangszustand
+    zustand = False
+    #GPIO.output(PIN, zustand)
+    print("Starte Zustand-Toggler (alle 60 Sekunden)...")
+
+    try:
+        while True:
+            # Zustand umschalten
+            switchGPIO()
+            print(f"Zustand gewechselt: {zustand}")
+            time.sleep(60)
+
+    except KeyboardInterrupt:
+        print("Programm manuell beendet.")
+
+    finally:
+        GPIO.cleanup()
+        print("GPIO zur├╝ckgesetzt.")
+
+def REST_API():
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
+# setup threading
+t_mainThread = threading.Thread(target=mainThread)
+t_REST_API = threading.Thread(target=REST_API)
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN, GPIO.OUT)
+    GPIO.output(PIN, zustand)
+    t_mainThread.start()
+    #t_REST_API.start()
+    REST_API()
+
