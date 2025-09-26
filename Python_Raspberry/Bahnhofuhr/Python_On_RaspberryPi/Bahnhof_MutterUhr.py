852613cb7da5808ec5e7a2d14e1661c5e803bece
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2025-09-26 20:36:14 +0200

    update

--- Python_Raspberry/Bahnhofuhr/Python_On_RaspberryPi/Bahnhof_MutterUhr.py
+++ Python_Raspberry/Bahnhofuhr/Python_On_RaspberryPi/Bahnhof_MutterUhr.py
@@ -20,20 +20,21 @@
 # 25-Aug-2025   Walter Rothlin      GPIO Test
 # 28-Aug-2025   Walter Rothlin      Added nice Frontend
 # 29-Aug-2025   Walter Rothlin      Startable via crontab, Uhr richten
+# 26-Sep-2025   Walter Rothlin      Added Lampe on/off Relais
 # ------------------------------------------------------------------
 import RPi.GPIO as GPIO
 import time
 import threading
-import time
-from   datetime import datetime
-from   flask import Flask, request, jsonify
+from datetime import datetime
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import socket
 
 # === Globale Variablen ===
-GPIO_PIN       = 26     # Pin-Definition (BCM-Nummerierung)
-current_state  = False  # Anfangszustand
+GPIO_PIN_Min_Clock = 26  # Pin-Definition for Min-Clock Relais (BCM-Nummerierung)
+GPIO_PIN_Lampe_On_Off = 19  # Pin-Definition for Lampe on/off     (BCM-Nummerierung)
+current_state = False  # Anfangszustand
 tick_controler = None
-aktion_aktiv   = True
+aktion_aktiv = True
 
 last_timer_config = {
     "func": None,
@@ -42,14 +43,15 @@ last_timer_config = {
     "kwargs": {"name": "SBB-Uhr", "wert": 0}
 }
 
+
 def switchGPIO():
     global current_state
     current_state = not current_state
     if current_state:
-        GPIO.output(GPIO_PIN, GPIO.LOW)
+        GPIO.output(GPIO_PIN_Min_Clock, GPIO.LOW)
     else:
-        GPIO.output(GPIO_PIN, GPIO.HIGH)
-    
+        GPIO.output(GPIO_PIN_Min_Clock, GPIO.HIGH)
+
 
 def tickArgs(count=10, delay=1):
     print('tick ', end='', flush=True)
@@ -59,28 +61,30 @@ def tickArgs(count=10, delay=1):
         time.sleep(delay)
     print()
     return "done"
-    
+
+
 def TEST_01():
     print('TEST_01 running....')
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIO_PIN, GPIO.OUT)
-    print(f'Set GPIO_PIN:{GPIO_PIN} to HIGH (Off)')
-    GPIO.output(GPIO_PIN, GPIO.HIGH)
+    GPIO.setup(GPIO_PIN_Min_Clock, GPIO.OUT)
+    print(f'Set GPIO_PIN_Min_Clock:{GPIO_PIN_Min_Clock} to HIGH (Off)')
+    GPIO.output(GPIO_PIN_Min_Clock, GPIO.HIGH)
     do_loop = True
     while do_loop:
         antwort = input('Weiter (s=stopp):')
         if antwort == 's':
-            do_loop=False
+            do_loop = False
         else:
             switchGPIO()
-    
+
     time.sleep(5)
-    
+
     tickArgs(count=5, delay=1)
-    
-    print(f'Set GPIO_PIN:{GPIO_PIN} to LOW (on)')
-    GPIO.output(GPIO_PIN, GPIO.LOW)
-    
+
+    print(f'Set GPIO_PIN_Min_Clock:{GPIO_PIN_Min_Clock} to LOW (on)')
+    GPIO.output(GPIO_PIN_Min_Clock, GPIO.LOW)
+
+
 # === Timer-Klasse ===
 class timer_controler:
     def __init__(self, func, interval_seconds=60, *args, **kwargs):
@@ -144,6 +148,7 @@ def meine_aktion(name, wert=0):
     switchGPIO()
     print(f"   ...ausgeführt!\n")
 
+
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -155,121 +160,49 @@ def get_ip():
     finally:
         s.close()
     return ip
-    
+
+
+# Zustand der Lampe, Clock_Started, Clock_Suspended merken
+lampen_status = False  # False = Aus, True = An
+clock_started = True
+clock_suspended = True
+
+
+def set_lampen_relais(state: bool):
+    global lampen_status
+    lampen_status = state
+    # Je nach Relais evtl. invertieren (HIGH = Aus, LOW = An)
+    GPIO.output(GPIO_PIN_Lampe_On_Off, GPIO.HIGH if state else GPIO.LOW)
+
 
 # === Flask Webserver ===
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/home/pi/Waltis_Repo_Clone/RaspberryPi4PiPlates/Python_Raspberry/Bahnhofuhr/Python_On_RaspberryPi/templates")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    uhr_bild_url = "https://www.blog.mobatime.ch/hs-fs/hubfs/Bilder/Blog/sbb_sekunden.gif?width=392&height=396&name=sbb_sekunden.gif"
     now = datetime.now()
-    javaScript_clock = '''
-          <!-- Bootstrap JS -->
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-          
-          <!-- JavaScript für Live-Uhr -->
-          <script>
-            function updateClock() {
-              const now = new Date();
-              const options = { 
-                day: '2-digit', month: 'long', year: 'numeric',
-                hour: '2-digit', minute: '2-digit', second: '2-digit'
-              };
-              document.getElementById("clock").textContent = now.toLocaleString("de-DE", options);
-            }
-            setInterval(updateClock, 1000);
-            updateClock(); // erste Anzeige sofort
-          </script>
-    '''
-
-    return f'''
-        <!DOCTYPE html>
-        <html lang="de">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <title>SBB-Uhr</title>
-          <!-- Bootstrap CSS -->
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        </head>
-        <body class="bg-light">
-
-          <div class="container py-5">
-            <div class="card shadow-lg p-4">
-              <div class="card-body text-center">
-                
-                <!-- Bild -->
-                <img src="https://www.blog.mobatime.ch/hs-fs/hubfs/Bilder/Blog/sbb_sekunden.gif?width=392&height=396&name=sbb_sekunden.gif" 
-                     class="img-fluid mx-auto d-block mb-3" 
-                     alt="SBB Uhr" 
-                     width="120">
-                
-                <h1 class="h4 mb-2">SBB-Uhr Steuerung</h1>
-                
-                <!-- Live Uhrzeit -->
-                <h2 class="h5 text-muted mb-4" id="clock"></h2>
-                <h2 class="h5 text-muted mb-4">{now.strftime('%d.%B %Y %H:%M:%S')}</h2>
-
-                <!-- Eingabe-Feld für Zeit -->
-                <div class="mt-4 mb-4">
-                  <form action="/set_time" method="post" class="row g-2 justify-content-center">
-                    <div class="col-auto d-flex align-items-center">
-                        <label for="user_time" class="form-label fw-bold">Uhr zeigt</label>
-                    </div>
-                    <div class="col-auto">
-                      <input type="time" name="user_time" class="form-control form-control-lg" required>
-                    </div>
-                    <div class="col-auto">
-                      <button type="submit" class="btn btn-outline-secondary btn-lg">⏰ Zeit setzen</button>
-                    </div>
-                  </form>
-                </div>
-
-                <!-- Buttons -->
-                <div class="row g-3">
-                  <div class="col-md-6">
-                    <form action="/start_timer" method="post">
-                      <button type="submit" class="btn btn-success btn-lg w-100">▶ Start Clock</button>
-                    </form>
-                  </div>
-                  <div class="col-md-6">
-                    <form action="/stop_timer" method="post">
-                      <button type="submit" class="btn btn-danger btn-lg w-100">⏹ Stop Clock</button>
-                    </form>
-                  </div>
-                  <div class="col-md-6">
-                    <form action="/suspend" method="post">
-                      <button type="submit" class="btn btn-warning btn-lg w-100">⏸ Suspend Tick</button>
-                    </form>
-                  </div>
-                  <div class="col-md-6">
-                    <form action="/activate" method="post">
-                      <button type="submit" class="btn btn-primary btn-lg w-100">⏵ Activate Tick</button>
-                    </form>
-                  </div>
-                </div>
-                
-              </div>
-                <!-- Status Button -->
-                <form action="/status_JSON" method="get" class="mb-4">
-                  <button type="submit" class="btn btn-outline-primary btn-lg">
-                    Status als JSON abrufen
-                  </button>
-                </form>
-            </div>
-          </div>
-          {javaScript_clock}
-        </body>
-        </html>
-    '''
+    print(f'lampe_an={lampen_status}, clock_started={clock_started}, clock_suspended={clock_suspended}')
+    return render_template("index.html",
+                           lampe_an=lampen_status,
+                           clock_started=clock_started,
+                           aktion_aktiv=aktion_aktiv,
+                           server_time=now.strftime("%d.%B %Y %H:%M:%S"))
 
 
 @app.route('/start_timer', methods=['POST'])
-def start_timer():
+def start_timer(direct_called=False):
+    global lampen_status
+    global clock_started
     global tick_controler, last_timer_config
+
+    clock_started = True
+
     if tick_controler and not tick_controler.stop_event.is_set():
-        return {"status": "already running", "message": "Timer läuft bereits."}
+        if direct_called:
+            return {"status": "already running", "message": "Timer läuft bereits."}
+        else:
+            return redirect(url_for("index"))
 
     tick_controler = timer_controler(
         last_timer_config["func"],
@@ -277,27 +210,41 @@ def start_timer():
         *last_timer_config["args"],
         **last_timer_config["kwargs"]
     )
-    return {"status": "started", "message": "Timer wurde gestartet."}
+
+    if direct_called:
+        return {"status": "already running", "message": "Timer läuft bereits."}
+    else:
+        return redirect(url_for("index"))
+
 
 @app.route('/stop_timer', methods=['POST'])
 def stop_timer():
+    global lampen_status
+    global clock_started
     global tick_controler
+
+    clock_started = False
+
     if tick_controler and not tick_controler.stop_event.is_set():
         tick_controler.stop()
-        return {"status": "stopped", "message": "Timer wurde gestoppt."}
-    return {"status": "already stopped", "message": "Timer ist bereits gestoppt."}
+        return redirect(url_for("index"))
+
 
 @app.route('/suspend', methods=['POST'])
 def suspend():
     global aktion_aktiv
     aktion_aktiv = False
-    return {"status": "suspended", "message": "Aktionen werden nicht mehr ausgeführt."}
+    # return {"status": "suspended", "message": "Aktionen werden nicht mehr ausgeführt."}
+    return redirect(url_for("index"))
+
 
 @app.route('/activate', methods=['POST'])
 def activate():
     global aktion_aktiv
     aktion_aktiv = True
-    return {"status": "active", "message": "Aktionen werden wieder ausgeführt."}
+    # return {"status": "active", "message": "Aktionen werden wieder ausgeführt."}
+    return redirect(url_for("index"))
+
 
 @app.route('/status_JSON', methods=['GET'])
 def status_JSON():
@@ -317,7 +264,8 @@ def status_JSON():
         "04_aktion_status": aktion_status,
         "05_interval_seconds": interval,
     }
-  
+
+
 @app.route("/set_time", methods=["POST"])
 def set_time():
     display_time = request.form.get("user_time")
@@ -326,43 +274,65 @@ def set_time():
         display_hour = int(display_hour) % 12
         display_total_minutes = display_hour * 60 + int(display_minutes)
         print(f"Angezeigte Zeit: {display_time}   {display_hour}::{display_minutes}  --> {display_total_minutes}")
-                
-                
+
         now = datetime.now()
         ist_time = f"{now.strftime('%H:%M')}"
         ist_hour, ist_minutes = ist_time.split(':')
         ist_hour = int(ist_hour) % 12
         ist_total_minutes = ist_hour * 60 + int(ist_minutes)
         print(f"Aktuelle Zeit: {ist_time}   {ist_hour}::{ist_minutes}  --> {ist_total_minutes}")
-        
-        count_of_forward_ticks = (ist_total_minutes - display_total_minutes) % (12*60)  # innerhalb von 12h
+
+        count_of_forward_ticks = (ist_total_minutes - display_total_minutes) % (12 * 60)  # innerhalb von 12h
         print(f"{count_of_forward_ticks} Minuten vorwärts stellen")
         if count_of_forward_ticks > 100:
             count_of_forward_ticks = 0
-            ret_str =  f"Zeitdifferenz zu gross!!! Suspend Ticks!"
+            ret_str = f"Zeitdifferenz zu gross!!! Suspend Ticks!"
         else:
-            ret_str =  f"Uhr {count_of_forward_ticks} Minuten vorgestellt!"
-            
+            ret_str = f"Uhr {count_of_forward_ticks} Minuten vorgestellt!"
+
         # z. B. an deine Timer-/GPIO-Logik weiterleiten
         tickArgs(count=count_of_forward_ticks, delay=1)
-    return ret_str
-    
-    
+    # return ret_str
+    return redirect(url_for("index"))
+
+
+@app.route('/set_lampe_on')
+def set_lampe_on():
+    set_lampen_relais(True)
+    return 'Lampe On'
+
+
+@app.route('/set_lampe_off')
+def set_lampe_off():
+    set_lampen_relais(False)
+    return 'Lampe Off'
+
+
+@app.route("/toggle_lampe", methods=["POST"])
+def toggle_lampe():
+    set_lampen_relais(not lampen_status)
+    return redirect(url_for("index"))
+
+
 if __name__ == '__main__':
     # TEST_01()
-    
+
     # Initialisiere Standard-Konfiguration
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIO_PIN, GPIO.OUT)
-    print(f'Set GPIO_PIN:{GPIO_PIN} to HIGH (Off)')
-    GPIO.output(GPIO_PIN, GPIO.HIGH)
-    last_timer_config["func"] = meine_aktion
+    GPIO.setup(GPIO_PIN_Min_Clock, GPIO.OUT)
+    print(f'Set GPIO_PIN_Min_Clock:{GPIO_PIN_Min_Clock} to HIGH (Off)')
+    GPIO.output(GPIO_PIN_Min_Clock, GPIO.HIGH)
 
-    start_timer()
+    GPIO.setup(GPIO_PIN_Lampe_On_Off, GPIO.OUT)
+    set_lampen_relais(True)
+    last_timer_config["func"] = meine_aktion
+    start_timer(direct_called=True)
 
     try:
         host_ip = get_ip()
         print(f"Starte Flask auf {host_ip}:5001")
+        time.sleep(3)
+        set_lampen_relais(False)
         # app.run(debug=True, host=host_ip, port=5001, use_reloader=False)
         app.run(host="0.0.0.0", port=5001, debug=False, use_reloader=False)
     except KeyboardInterrupt:
